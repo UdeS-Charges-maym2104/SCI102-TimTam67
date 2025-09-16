5bdc91b343a48a29b3c3bb4c876ec5046f2af1b6
TimTam67 <timorat06@gmail.com>
2025-09-16 15:24:49 -0400

    encore fonction diff

--- labo2.py
+++ labo2.py
@@ -1,7 +1,7 @@
 
 def age(x):
     y=2025-x
-    return f"Votre age est {y} ans."
+    return f"Votre âge est {y} ans."
   
 def salutations(noms):
-    return f"Bonjour {noms}!"
+    return f"Bonjour {noms}."
